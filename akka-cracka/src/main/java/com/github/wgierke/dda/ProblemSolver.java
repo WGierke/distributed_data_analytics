3bbad2011423d83a5eead67c25ad0f8318690afa
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2017-12-08 17:53:10 +0100

    Remove support for actor switch

--- akka-cracka/src/main/java/com/github/wgierke/dda/ProblemSolver.java
+++ akka-cracka/src/main/java/com/github/wgierke/dda/ProblemSolver.java
@@ -5,55 +5,19 @@ import akka.actor.ActorSystem;
 import com.github.wgierke.dda.actors.Master;
 import com.github.wgierke.dda.actors.Writer;
 import com.github.wgierke.dda.messages.AnalyseStudentsMessage;
-import com.github.wgierke.dda.messages.ShutdownMessage;
 
 public class ProblemSolver {
     public static void main(String[] args) {
-        String command = (args.length == 1) ? args[0] : "all";
-
-        switch (command) {
-            case "pw":
-                runCracker();
-                break;
-            case "gene":
-                runGeneChecker();
-                break;
-            case "all":
-                runAll();
-                break;
-            default:
-                System.out.println("Bad command! Need to provide 'pw', 'gene', 'all', or no command.");
-                System.exit(1);
-        }
+        String path = args[0];
+        runAll(path);
     }
 
-    private static void runCracker() {
-        System.out.println("Running Password AnalyseStudentsMessage :)");
+    private static void runAll(String studentsFilePath) {
+        //TODO: add support for path argument
+        System.out.println("Running PasswordCracker and GeneChecker :)");
         final ActorSystem actorSystem = ActorSystem.create("Cracker");
         ActorRef listener = actorSystem.actorOf(Writer.props(System.out));
         ActorRef master = actorSystem.actorOf(Master.props(listener));
-
         master.tell(new AnalyseStudentsMessage(), ActorRef.noSender());
-
-//        try {
-//            Thread.sleep(10 * 1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        master.tell(new ShutdownMessage(), ActorRef.noSender());
-    }
-
-    private static void runGeneChecker() {
-        System.out.println("Running Gene Check :)");
-        final ActorSystem actorSystem = ActorSystem.create("GeneChecker");
-        ActorRef listener = actorSystem.actorOf(Writer.props(System.out));
-        ActorRef master = actorSystem.actorOf(Master.props(listener));
-
-        master.tell(new AnalyseStudentsMessage(), ActorRef.noSender());
-    }
-
-    private static void runAll() {
-        System.out.println("Running all :)");
     }
 }
