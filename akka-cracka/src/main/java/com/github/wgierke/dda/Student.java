b5f885cec8bed8a85d9864fc2f6ad7aebee14317
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2017-12-08 16:07:44 +0100

    Add logic for password cracking

--- akka-cracka/src/main/java/com/github/wgierke/dda/Student.java
+++ akka-cracka/src/main/java/com/github/wgierke/dda/Student.java
@@ -5,12 +5,14 @@ public class Student {
     final private String name;
     final private String hash;
     final private String genes;
+    private String password;
 
     public Student(int id, String name, String hash, String genes) {
         this.id = id;
         this.name = name;
         this.hash = hash;
         this.genes = genes;
+        this.password = null;
     }
 
 
@@ -28,5 +30,13 @@ public class Student {
 
     public String getGenes() {
         return genes;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- akka-cracka/src/main/java/com/github/wgierke/dda/actors/Cracker.java
+++ akka-cracka/src/main/java/com/github/wgierke/dda/actors/Cracker.java
@@ -6,12 +6,24 @@ import com.github.wgierke.dda.Student;
 import com.github.wgierke.dda.messages.CrackerMessage;
 import com.github.wgierke.dda.messages.PasswordCrackedMessage;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
 public class Cracker extends AbstractLoggingActor {
     private void crack(CrackerMessage crackerMessage) {
         Student student = crackerMessage.getStudent();
         this.log().debug("Received crack challenge for student " + student.getName() + " (ID: " + student.getId() + ")");
 
-        this.getSender().tell(new PasswordCrackedMessage(student, "0000000"), this.getSelf());
+        for(int i = 1000000; i <= 9999999; i++) {
+            if(Objects.equals(student.getHash(), hash(String.valueOf(i)))) {
+                student.setPassword(String.valueOf(i));
+                break;
+            }
+        }
+
+        this.getSender().tell(new PasswordCrackedMessage(student, student.getPassword()), this.getSelf());
     }
 
     @Override
@@ -24,5 +36,24 @@ public class Cracker extends AbstractLoggingActor {
 
     public static Props props() {
         return Props.create(Cracker.class);
+    }
+
+    private String hash(String line) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("SHA-256 is not supported :-(");
+        }
+        byte[] hashedBytes = new byte[0];
+        try {
+            hashedBytes = digest.digest(line.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("SHA-256 encoding is not supported :-(");
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < hashedBytes.length; i++)
+            stringBuffer.append(Integer.toString((hashedBytes[i] & 0xff) + 0x100, 16).substring(1));
+        return stringBuffer.toString();
     }
 }
